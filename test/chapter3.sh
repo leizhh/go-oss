5a837ca60149f49271efbb603c3eba66585c4969
ubuntu <1044657730@qq.com>
2020-05-05 15:24:40 +0800

    add test

--- test/chapter3.sh
+++ test/chapter3.sh
@@ -11,12 +11,14 @@ echo
 echo -n "this is object test3 version 2" | openssl dgst -sha256 -binary | base64
 curl -v 127.0.0.1:30000/objects/test3 -XPUT -d"this is object test3 version 2" -H "Digest: SHA-256=cAPvsxZe1PR54zIESQy0BaxC1pYJIvaHSF3qEOZYYIo="
 
+curl -v 127.0.0.1:30000/objects/test3 -XPUT -d"this is object test3 version 3" -H "Digest: SHA-256=v8EJIZMsSfWXGdrlV2dFe4wUkinaWN6f1ql6cOu1KWA="
+
 curl 127.0.0.1:30000/objects/test3
 echo
 
 curl 127.0.0.1:30000/objects/test3
 echo
-curl 127.0.0.1:30000/locate/GYqqAdFPt+CScnUDc0%2FGcu3kwcWmOADKNYpiZtdbgs=
+curl 127.0.0.1:30000/locate/GYqqAdFPt+CScnUDc0%2FGcu3kwcWmOADKNYpiZtdbgsM=
 echo
 curl 127.0.0.1:30000/locate/cAPvsxZe1PR54zIESQy0BaxC1pYJIvaHSF3qEOZYYIo=
 echo
--- test/chapter4.sh
+++ test/chapter4.sh
@@ -4,7 +4,7 @@ echo -n "this object will have only 1 instance" | openssl dgst -sha256 -binary |
 
 curl -v 127.0.0.1:30000/objects/test4_1 -XPUT -d "this object will have only 1 instance" -H "Digest: SHA-256=incorrecthash"
 
-curl -v 127.0.0.1:30000/objects/test4_1 -XPUT -d "this object will have only 1 instance" -H "Digest: SHA-256=aWKQ2BipX94sb+h3xdTbWYAu1yzjn5vyFG2SOwUQIXY="
+curl -v 127.0.0.1:30000/objects/-9-XPUT -d "this object will have only 1 instance" -H "Digest: SHA-256=aWKQ2BipX94sb+h3xdTbWYAu1yzjn5vyFG2SOwUQIXY="
 
 curl -v 127.0.0.1:30000/objects/test4_2 -XPUT -d "this object will have only 1 instance" -H "Digest: SHA-256=aWKQ2BipX94sb+h3xdTbWYAu1yzjn5vyFG2SOwUQIXY="
 
